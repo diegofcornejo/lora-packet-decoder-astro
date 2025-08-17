117e3b1ea5e64641ec0974abbf3497e0ef52deaa
Diego Cornejo <diegof.cornejo@gmail.com>
2025-08-16 21:25:45 -0600

    chore: Update dependencies and clean up Astro configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,7 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
-import node from "@astrojs/node";
-
-import vercel from "@astrojs/vercel/serverless";
+import vercel from "@astrojs/vercel";
 
 // https://astro.build/config
 export default defineConfig({
